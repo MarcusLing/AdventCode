c5b6694dafe6511b14e79094a28a2e0f1f5aadf9
Marcus Ling <marcus.ling@moneygo.se>
2019-12-06 13:21:58 +0100

    Update app.d

--- source/app.d
+++ source/app.d
@@ -2,6 +2,6 @@ import std.stdio, ImportInput;
 
 void main()
 {
-	auto test = new ImportInput("https://adventofcode.com/2019/day/1/input","53616c7465645f5fca203c4069d74af6153ea60d1c4bfdfb630767627f413a620e9cfa54e04487bb032df40f1ed02bc8");
+	auto test = new ImportInput("https://adventofcode.com/2019/day/1/input","");
 	test.getInput();
 }
